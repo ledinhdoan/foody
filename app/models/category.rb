5f494e18bb918c94ab81881a2fb09938e5957763
lexuangiang <xuangiangkt94@gmail.com>
2016-10-20 10:47:28 +0700

    show category in nav, product in view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+
+  scope :asc_by_name, ->{order :name}
+  scope :limit_category, ->{limit Settings.category.limit}
 end
