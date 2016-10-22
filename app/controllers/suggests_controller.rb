64170651de3280c958939899d7ad47c0d312f168
lexuangiang <xuangiangkt94@gmail.com>
2016-10-22 15:17:45 +0700

    suggest food

---
+++ app/controllers/suggests_controller.rb
@@ -0,0 +1,62 @@
+class SuggestsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_suggest, only: [:show, :update, :edit, :destroy]
+
+
+  def index
+    @suggests = Suggest.all
+  end
+
+  def show
+  end
+
+  def new
+    @suggest = Suggest.new
+  end
+
+  def create
+    @suggest = Suggest.new suggest_params
+    if @suggest.save
+      flash[:success] = "done"
+      redirect_to @suggest
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @suggest.update_attribute suggest_params
+      flash.now[:success] = "done"
+      redirect_to @suggest
+    else
+      flash.now[:warning] = "fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @suggest.destroy
+      flash[:success] = "done"
+      redirect_to suggests_path
+    else
+      flash.now[:warning] = "fail"
+      redirect_to root_path
+    end
+  end
+
+  private
+  def load_suggest
+    @suggest = Suggest.find_by id: params[:id]
+    unless @suggest
+      flash.now[:warning] = "not_found"
+      redirect_to root_path
+    end
+  end
+
+  def suggest_params
+    params.require(:suggest).permit :name, :body, :image, :price, :status
+  end
+end
