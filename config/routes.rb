b3cd082c34636f5ddd5f02a477be433c8370d51a
lexuangiang <xuangiangkt94@gmail.com>
2016-10-23 22:47:32 +0700

    dev comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     root "admin/users#index"
     resources :users
   end
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :comments
+  end
   resources :categories, only: [:index, :show]
   resources :suggests
 end
